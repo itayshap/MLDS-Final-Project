5671ab3d3475bc2f2b1653e0fd8b8bbe79c58e33
itay shapira <itayshapira@hotmail.com>
2024-08-20 21:04:23 +0300

    use ModelPath enum and TrainParams in builder

--- Utils/Model_Builder.py
+++ Utils/Model_Builder.py
@@ -1,43 +1,45 @@
 
 import torch
 from Enums.ModelType import ModelType
+from Enums.ModelPath import ModelPath
 from modules.HeadlessPretrainedModule import HeadlessPretrainedModule
 from torchvision import models
 from modules.CustomModule import CustomModule2
 from modules.VitModule import VitModule
+from modules.BaseModule import BaseModule
 from os.path import isfile
 from collections import namedtuple
 from torch import nn
 from torch.nn import Module
 from torch.utils.data import DataLoader
+from Config import TrainParams
 
 from transforms.Transform_Builder import Transform_Builder
 
-namedtuple('TrainParams',['lr', 'num_epochs'])
 class Model_Builder():
 
     @staticmethod
-    def build(modelType: ModelType, device: str, trainParams: namedtuple = None, dataloaders: dict = None):
+    def build(modelType: ModelType, device: str, trainParams: TrainParams = None, dataloaders: dict = None, path :ModelPath = ModelPath.BASELINE_CUSTOM):
         if modelType==ModelType.PRETRAINED:
             model = HeadlessPretrainedModule(pretrained_model = models.resnet50(pretrained=True), device=device)
             model = model.to(device)
-            Model_Builder.load_or_train_model(model, "./pretrained_model.pth", dataloaders, trainParams)
+            Model_Builder.load_or_train_model(model, path, dataloaders, trainParams)
             
         elif modelType==ModelType.CUSTOM:
             model = CustomModule2(device=device)
             model = model.to(device)
-            model = Model_Builder.load_or_train_model(model, "./CustomModel.pth", dataloaders, trainParams)
+            model = Model_Builder.load_or_train_model(model, path, dataloaders, trainParams)
         else:
             model = VitModule('nateraw/vit-base-cats-vs-dogs', device)
         return model
 
-    def load_or_train_model(model: Module, path: str, dataloaders: dict, trainParams: namedtuple):
+    def load_or_train_model(model: BaseModule, path: str, dataloaders: dict, trainParams: TrainParams):
         if isfile(path):
             model.load_state_dict(torch.load(path))
         else:
             model_optimizer = trainParams.optimizer(model.parameters(), trainParams.lr)
             criterion = nn.CrossEntropyLoss()
-            model.start_train(criterion, model_optimizer, dataloaders, num_epochs=trainParams.num_epochs)
+            model.start_train(criterion, model_optimizer, dataloaders=dataloaders, num_epochs=trainParams.num_epochs, verbose=trainParams.verbose)
             torch.save(model.state_dict(), path)
         return model
     
