2ccb6d6b43a359e0b49e812b75f811ef67656c12
itay shapira <itayshapira@hotmail.com>
2024-09-11 12:55:04 +0300

    new weights and weights names

--- Enums/ModelPath.py
+++ Enums/ModelPath.py
@@ -1,11 +1,14 @@
 from enum import Enum
 
 class ModelPath(Enum):
-    BASELINE_PRETRAINED = "./pretrained_model.pth"
+    BASELINE_PRETRAINED = "./Pretrained_Model.pth"
     BASELINE_CUSTOM = "./CustomModel.pth"
+    BASELINE_IMPROVED_CUSTOM = "./Improved_CustomModel.pth"
 
-    PERMUTATION_FT_PRETRAINED = './pretrained_model_premutation.pth'
-    PERMUTATION_FT_CUSTOM = './CustomModel_premutation.pth'
+    PERMUTATION_FT_PRETRAINED = './Pretrained_Model_Premutation.pth'
+    PERMUTATION_FT_CUSTOM = './CustomModel_Premutation.pth'
+    PERMUTATION_FT_IMPROVED_CUSTOM = './Improved_CustomModel_Premutation.pth'
+    SEVERAL_PERMUTATION_FT_IMPROVED_CUSTOM = './Improved_CustomModel_SeveralPremutations.pth'
     PERMUTATION_FT_VIT = './Vit_premutation_classifier_layer.pth'
 
     
