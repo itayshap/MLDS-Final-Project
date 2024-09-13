f3148143c8c81fbc8025318f88120abf9d22990a
itay shapira <itayshapira@hotmail.com>
2024-09-13 14:34:42 +0300

    Moved weights to Weights folder

--- Enums/ModelPath.py
+++ Enums/ModelPath.py
@@ -1,14 +1,15 @@
 from enum import Enum
 
 class ModelPath(Enum):
-    BASELINE_PRETRAINED = "./Pretrained_Model.pth"
-    BASELINE_CUSTOM = "./CustomModel.pth"
-    BASELINE_IMPROVED_CUSTOM = "./Improved_CustomModel.pth"
+    BASELINE_PRETRAINED = "./Weights/Pretrained_Model.pth"
+    BASELINE_CUSTOM = "./Weights/CustomModel.pth"
+    BASELINE_IMPROVED_CUSTOM = "./Weights/Improved_CustomModel.pth"
+    
 
-    PERMUTATION_FT_PRETRAINED = './Pretrained_Model_Premutation.pth'
-    PERMUTATION_FT_CUSTOM = './CustomModel_Premutation.pth'
-    PERMUTATION_FT_IMPROVED_CUSTOM = './Improved_CustomModel_Premutation.pth'
-    SEVERAL_PERMUTATION_FT_IMPROVED_CUSTOM = './Improved_CustomModel_SeveralPremutations.pth'
-    PERMUTATION_FT_VIT = './Vit_premutation_classifier_layer.pth'
+    PERMUTATION_FT_PRETRAINED = './Weights/Pretrained_Model_Premutation.pth'
+    PERMUTATION_FT_CUSTOM = './Weights/CustomModel_Premutation.pth'
+    PERMUTATION_FT_IMPROVED_CUSTOM = './Weights/Improved_CustomModel_Premutation.pth'
+    SEVERAL_PERMUTATION_FT_IMPROVED_CUSTOM = './Weights/Improved_CustomModel_SeveralPremutations.pth'
+    PERMUTATION_FT_VIT = './Weights/Vit_premutation_classifier_layer.pth'
 
     
